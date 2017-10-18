25375537bee3e043b29fca79bbe4bb479b175c29
yijiecat <yijiecat@gmail.com>
2017-10-18 12:43:47 +0800

    新增Udemy_Puzzles_08_Game_Of_Craps

---
+++ Udemy/Udemy_Puzzles_08_Game_Of_Craps/Udemy_Puzzles_08_Game_Of_Craps/CrapsGame.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Puzzles_08_Game_Of_Craps
+{
+    public class CrapsGame
+    {
+        private enum DiceSum
+        {
+            SnakeEyes = 2,
+            Trey = 3,
+            Seven = 7,
+            YoLeven = 11,
+            BoxCars = 12
+        };
+
+        private enum GameStatus
+        {
+            Win,
+            Lose,
+            Continue
+        };
+
+        private RollDice _roll = new RollDice();
+        private GameStatus _gameStatus;
+        private DiceSum _diceSum;
+        private int _numRolls;
+        private int _sum;
+        private int _point;
+
+        private void EvaluateRoll()
+        {
+            switch (_sum)
+            {
+                case 7:
+                    _diceSum = DiceSum.Seven;
+                    _gameStatus = GameStatus.Win;
+                    _point = 0;
+                    break;
+                case 11:
+                    _diceSum = DiceSum.YoLeven;
+                    _gameStatus = GameStatus.Win;
+                    _point = 0;
+                    break;
+                case 2:
+                    _diceSum = DiceSum.SnakeEyes;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                case 3:
+                    _diceSum = DiceSum.Trey;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                case 12:
+                    _diceSum = DiceSum.BoxCars;
+                    _gameStatus = GameStatus.Lose;
+                    _point = 0;
+                    break;
+                default:
+                    _gameStatus = GameStatus.Continue;
+                    _point = _sum;
+                    break;
+            }
+        }
+        private void DisplayMessage()
+        {
+            switch (_gameStatus)
+            {
+                case GameStatus.Win:
+                    Console.WriteLine(_numRolls == 1
+                        ? $"Congratulations, you rolled {_diceSum}. YOU WIN!! "
+                        : $"Congratulations, you rolled {_sum}. YOU WIN!! ");
+                    break;
+                case GameStatus.Lose:
+                    Console.WriteLine(_numRolls == 1
+                        ? $"Sorry, you rolled {_diceSum}. YOU LOSE !! "
+                        : $"Congratulations, you rolled {_sum}. YOU WIN!! ");
+                    break;
+                default:
+                    Console.WriteLine($"You rolled {_sum}. Your point is {_point}. Keep rolling! ");
+                    break;
+
+            }
+        }
+        public void Play()
+        {
+            _sum = _roll.DiceRoll();
+            _numRolls++;
+            EvaluateRoll();
+            DisplayMessage();
+
+            while (_gameStatus == GameStatus.Continue)
+            {
+                KeepPlaying();
+                DisplayMessage();
+            }
+        }
+
+        private void KeepPlaying()
+        {
+            _sum = _roll.DiceRoll();
+            _numRolls++;
+
+            if (_sum == _point)
+                _gameStatus = GameStatus.Win;
+            else if (_sum == 7)
+                _gameStatus = GameStatus.Lose;
+            else
+                _gameStatus = GameStatus.Continue;
+
+        }
+    }
+}
---
+++ Udemy/Udemy_Puzzles_08_Game_Of_Craps/Udemy_Puzzles_08_Game_Of_Craps/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Puzzles_08_Game_Of_Craps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrapsGame craps = new CrapsGame();
+            craps.Play();
+
+        }
+    }
+}
